1545a8a832fa93b260edaaa51e406cc25115557b
Yorsh <george.hartman@eagles.oc.edu>
2025-02-13 15:21:46 -0600

    Formatting and restricitng login showing

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <template>
   <v-app style="background-color: #242424;">
     <v-row no-gutters no-wrap>
-      <NavigationBar />
+      <NavigationBar v-if="$route.path !== '/login'"/>
       <v-main>
         <router-view />
       </v-main>
