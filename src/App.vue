d1731e180b123f7a4881179593d26234d6db4961
Yorsh <george.hartman@eagles.oc.edu>
2025-02-13 16:09:15 -0600

    Start of user card

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import { userStore } from "./stores/userStore";
 const store = userStore();
 store.setupRouteWatcher();
+import UserCard from "./components/UserCard.vue";
 </script>
 
 <template>
   <v-app>
     <!-- <MenuBar :key="$route.fullPath" /> -->
     <v-main color="background">
-      <router-view />
+<user-card></user-card>
     </v-main>
   </v-app>
 </template>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+
+<template>
+
+    <v-card class="userCard">
+      <img src="/Birb.png" class="userImg">
+      <p class="name">Jonah Veit</p>
+      <p class="grade">Sophomore</p>
+      <p class="major">Computer Science</p>
+    </v-card>
+
+</template>
+
+<style>
+
+.userCard {
+    display: flex;
+    height: 20vh;
+    width: 40vh;
+    margin: 20px;
+    border-radius: 25px;
+    border-left: 10px #f00 solid;
+}
+
+.userImg {
+    width: 33%;
+    height: 50%;
+    align-self: center;
+}
+
+.name {
+    width: 66%;
+    height: 33%;
+    
+}
+
+</style>
