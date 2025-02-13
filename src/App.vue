6ac520e9d3734161fe356aed7743b0818e082aa7
Frostbit912 <97480950+JonathanBooker912@users.noreply.github.com>
2025-02-13 14:13:07 -0600

    Added ESLint Config

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<script setup></script>
+<script setup>
+    import { userStore } from "./stores/userStore";
+    const store = userStore();
+    store.setupRouteWatcher();
+</script>
 
 <template>
-  <v-app>
-    <!-- <MenuBar :key="$route.fullPath" /> -->
-    <v-main color="background">
-      <router-view />
-    </v-main>
-  </v-app>
+    <v-app>
+        <!-- <MenuBar :key="$route.fullPath" /> -->
+        <v-main color="background">
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router/router.js";
 import vuetify from "./plugins/vuetify.js";
 import { createPinia } from "pinia";
-import { userStore } from "./stores/userStore.js";
 
 const app = createApp(App);
 const pinia = createPinia();
