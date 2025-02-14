600fff33aece9f4cd6f2fd17923425c55599f370
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-02-13 18:28:15 -0600

    Ran formatting

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -5,52 +5,51 @@ import roleServices from "../services/roleServices";
 import authServices from "../services/authServices";
 
 export const userStore = defineStore("user", {
-    state: () => ({
-        user: null,
-        roles: null,
-        currentRoute: null
-    }),
-    actions: {
-        async checkRole(roleName) {
-            // If the roles are null because of a reload, then the roles need to be requeried from the database
-            if (!this.roles) {
-                await this.setupStore();
-            }
+  state: () => ({
+    user: null,
+    roles: null,
+    currentRoute: null,
+  }),
+  actions: {
+    async checkRole(roleName) {
+      // If the roles are null because of a reload, then the roles need to be requeried from the database
+      if (!this.roles) {
+        await this.setupStore();
+      }
 
-            return this.roles
-                ? this.roles.some(
-                      (role) =>
-                          role.name.toLowerCase() === roleName.toLowerCase()
-                  )
-                : false;
-        },
-        async isAdmin() {
-            console.log("Inside isAdmin");
-            return await this.checkRole("admin");
-        },
-        async isFaculty() {
-            console.log("Inside isFaculty");
-            return await this.checkRole("faculty");
-        },
-        async isAuthenticated() {
-            if (!this.user) this.$patch({ user: Utils.getStore("user") });
-            try {
-                const { data } = await authServices.validateToken(this.user);
-                return data.isValid;
-            } catch (error) {
-                console.log("Token validation failed:", error);
-                return false;
-            }
-        },
-        async setupStore() {
-            const user = Utils.getStore("user");
-            let roles = [];
-            if (user) {
-                roles = await roleServices.getRolesByEmail(user.email);
-            }
-
-            this.$patch({ user, roles: roles.data });
-        }
+      return this.roles
+        ? this.roles.some(
+            (role) => role.name.toLowerCase() === roleName.toLowerCase(),
+          )
+        : false;
     },
-    persist: true // Persist across page reloads to keep the number of network requests down
+    async isAdmin() {
+      console.log("Inside isAdmin");
+      return await this.checkRole("admin");
+    },
+    async isFaculty() {
+      console.log("Inside isFaculty");
+      return await this.checkRole("faculty");
+    },
+    async isAuthenticated() {
+      if (!this.user) this.$patch({ user: Utils.getStore("user") });
+      try {
+        const { data } = await authServices.validateToken(this.user);
+        return data.isValid;
+      } catch (error) {
+        console.log("Token validation failed:", error);
+        return false;
+      }
+    },
+    async setupStore() {
+      const user = Utils.getStore("user");
+      let roles = [];
+      if (user) {
+        roles = await roleServices.getRolesByEmail(user.email);
+      }
+
+      this.$patch({ user, roles: roles.data });
+    },
+  },
+  persist: true, // Persist across page reloads to keep the number of network requests down
 });
