65de87ad13edf734a6417a8f4076631c00c3e178
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-02-16 14:24:10 -0600

    Fixed userStore weirdness

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
 </script>
 
 <template>
-    <v-app>
-        <!-- <MenuBar :key="$route.fullPath" /> -->
-        <v-main color="background">
-            <router-view />
-        </v-main>
-    </v-app>
+  <v-app>
+    <!-- <MenuBar :key="$route.fullPath" /> -->
+    <v-main color="background">
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
--- src/views/admin/AdminLanding.vue
+++ src/views/admin/AdminLanding.vue
@@ -1,7 +1,7 @@
 <script setup>
-    import { RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 <template>
-    <h1>Admin Landing Page</h1>
-    <router-view />
+  <h1>Admin Landing Page</h1>
+  <router-view />
 </template>
