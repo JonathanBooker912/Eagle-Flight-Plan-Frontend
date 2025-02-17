6b6ab34347e79a16dc22133d4d5336212f80a5c4
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-02-17 13:38:37 -0600

    Ran prettier

--- src/views/admin/TaskCardPage.vue
+++ src/views/admin/TaskCardPage.vue
@@ -1,21 +1,20 @@
 <script setup>
-    import { ref } from "vue";
-    import TaskCard from "../../components/cards/TaskCard.vue";
+import { ref } from "vue";
+import TaskCard from "../../components/cards/TaskCard.vue";
 
-    const task = ref({
-        category: "Academic",
-        taskType: "Automatic",
-        reflectionRequired: true,
-        schedulingType: "one-time",
-        completionType: "self-reported",
-        description:
-            "Build your profressional resume for potential job appilcations",
-        name: "Build Resume",
-        rationale: "Prepare student for career success",
-        points: 30
-    });
+const task = ref({
+  category: "Academic",
+  taskType: "Automatic",
+  reflectionRequired: true,
+  schedulingType: "one-time",
+  completionType: "self-reported",
+  description: "Build your profressional resume for potential job appilcations",
+  name: "Build Resume",
+  rationale: "Prepare student for career success",
+  points: 30,
+});
 </script>
 <template>
-    <TaskCard :task="task"></TaskCard>
-    <TaskCard :task="task"></TaskCard>
+  <TaskCard :task="task"></TaskCard>
+  <TaskCard :task="task"></TaskCard>
 </template>
