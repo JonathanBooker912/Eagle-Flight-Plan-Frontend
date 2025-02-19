c4585e994ab65fc6fd5fd8d57baee541b18ba5fa
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-02-19 14:55:19 -0600

    Added the add and edit pages plus all functionality for tasks

--- src/services/taskServices.js
+++ src/services/taskServices.js
@@ -9,6 +9,27 @@ export default {
       },
     });
   },
+  getTask(taskId) {
+    return apiClient.get(`/task/${taskId}`);
+  },
+  getCategories() {
+    return apiClient.get("/task/types/categories");
+  },
+  getSchedulingTypes() {
+    return apiClient.get("/task/types/schedulingTypes");
+  },
+  getTaskTypes() {
+    return apiClient.get("/task/types/taskTypes");
+  },
+  getCompletionTypes() {
+    return apiClient.get("/task/types/completionTypes");
+  },
+  createTask(taskData) {
+    return apiClient.post("/task", taskData);
+  },
+  updateTask(taskId, taskData) {
+    return apiClient.put(`/task/${taskId}`, taskData);
+  },
   deleteTask(taskId) {
     return apiClient.delete(`/task/${taskId}`);
   },
