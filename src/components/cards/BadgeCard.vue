15b28fc2b1427e9436e287244ee37ef8a2b3808a
jonahveit2002 <97561214+jonahveit2002@users.noreply.github.com>
2025-02-20 16:20:46 -0600

    badge card on mock badge page

--- src/components/cards/BadgeCard.vue
+++ src/components/cards/BadgeCard.vue
@@ -2,38 +2,30 @@
 const props = defineProps({
   badge: Object,
 });
+
 const emit = defineEmits(["edit", "delete"]);
+
 </script>
 <template>
   <v-card color="backgroundDarken" class="cardContainer">
-    <v-card-text>
-      <img src="../../assets/goldBadgeMock.png" width="100" height="100">
+    <v-card-text class="text-center" style="padding-bottom: 0px;">
+      <img src="../../assets/goldBadgeMock.png" width="100" class="badgeImage">
       <p class="text-h5 mb-2">
-          {{ props.badge.name }}  
+        {{ (props.badge.name.trim().length > 20) ? props.badge.name.trim().substring(0, 20) + '...' : props.badge.name.trim() }}  
       </p>
     </v-card-text>
-    <v-row class="ma-2 float-right">
-      <!-- <v-btn
-        color="warning"
-        class="mr-2 cardButton"
-        @click="emit('edit', props.badge.id)"
-      > -->
+    <v-row class="ma-2 actionButtons">
       <v-btn
         color="warning"
         class="mr-2 cardButton"
-        @click="emit('edit')"
+        @click="emit('edit', props.badge.id)"
       >
         <v-icon icon="mdi-pencil" color="text" size="x-large"></v-icon>
       </v-btn>
-      <!-- <v-btn
+      <v-btn
         color="danger"
         class="cardButton"
         @click="emit('delete', props.badge.id)"
-        > -->
-        <v-btn
-        color="danger"
-        class="cardButton"
-        @click="emit('delete')"
         >
         <v-icon icon="mdi-delete" color="text" size="x-large"></v-icon
       ></v-btn>
@@ -42,11 +34,27 @@ const emit = defineEmits(["edit", "delete"]);
 </template>
 
 <style scoped>
+
 .cardContainer {
-  min-width: 280px;
+  justify-items: center;
+  margin: 10px;
+  display: block;
+  width: 300px;
+  height: auto;
   border-radius: 25px;
 }
+
 .cardButton {
   border-radius: 13px;
+}
+
+.badgeImage {
+  display: block; 
+  margin: 0 auto; 
+}
+
+.actionButtons {
+  justify-content: center;
+  padding-bottom: 10px;
 }
 </style>
--- src/views/admin/BadgeCardPage.vue
+++ src/views/admin/BadgeCardPage.vue
@@ -4,8 +4,15 @@ import BadgeCard from "../../components/cards/BadgeCard.vue";
 const badge = ref({
   name: "Badge Name test",
 });
+const badge2 = ref({
+  name: "Badge Name test and this is soooo long",
+});
+
 </script>
 <template>
+  <BadgeCard :badge="badge"></BadgeCard>
+  <BadgeCard :badge="badge2"></BadgeCard>
+  <BadgeCard :badge="badge"></BadgeCard>
   <BadgeCard :badge="badge"></BadgeCard>
   <BadgeCard :badge="badge"></BadgeCard>
 </template>
