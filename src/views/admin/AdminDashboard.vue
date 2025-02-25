962470d298aa43d88b471a445feb7d59c5dbda09
Frostbit912 <97480950+JonathanBooker912@users.noreply.github.com>
2025-02-25 09:14:55 -0600

    migrated dummy pages

---
+++ src/views/admin/AdminDashboard.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <h1>Admin Dashboard Page</h1>
+</template>
---
+++ src/views/admin/AdminFlightPlan.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <h1>Admin Flight Plan Page</h1>
+</template>
