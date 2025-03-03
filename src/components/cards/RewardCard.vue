71e8076a9344ed11f63e3d22addab7ceb34fcf79
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-03-02 21:47:38 -0600

    Ran linting and formating

--- src/components/cards/RewardCard.vue
+++ src/components/cards/RewardCard.vue
@@ -1,100 +1,86 @@
 <script setup>
-    import { ref, onMounted, onUnmounted } from "vue";
-    import defaultImage from "/defaultRewardImage.png";
+import { ref, onMounted, onUnmounted } from "vue";
+import defaultImage from "/defaultRewardImage.png";
 
-    // Define statements for vue
-    const props = defineProps({
-        reward: { type: Object, required: true }
-    });
-    const emit = defineEmits(["edit", "delete", "shop"]);
+// Define statements for vue
+const props = defineProps({
+  reward: { type: Object, required: true },
+});
+const emit = defineEmits(["edit", "delete", "shop"]);
 
-    // Reactive states
-    const imageSrc = ref("");
+// Reactive states
+const imageSrc = ref("");
 
-    // Functions
-    const loadImage = (image) => {
-        if (!image || !image.data) return;
+// Functions
+const loadImage = (image) => {
+  if (!image || !image.data) return;
 
-        // Ensure image.data is a Uint8Array
-        const byteArray = new Uint8Array(image.data);
+  // Ensure image.data is a Uint8Array
+  const byteArray = new Uint8Array(image.data);
 
-        const blob = new Blob([byteArray], { type: image.type }); // Adjust type accordingly
-        imageSrc.value = URL.createObjectURL(blob);
-    };
+  const blob = new Blob([byteArray], { type: image.type }); // Adjust type accordingly
+  imageSrc.value = URL.createObjectURL(blob);
+};
 
-    // Vue functions
-    onMounted(() => {
-        loadImage(props.reward.image);
-    });
-    onUnmounted(() => URL.revokeObjectURL(imageSrc.value));
+// Vue functions
+onMounted(() => {
+  loadImage(props.reward.image);
+});
+onUnmounted(() => URL.revokeObjectURL(imageSrc.value));
 </script>
 <template>
-    <v-card color="backgroundDarken" class="cardContainer">
-        <v-card-text>
-            <v-img
-                v-if="imageSrc"
-                class="image"
-                :src="imageSrc"
-                alt="Uploaded Image"
-            ></v-img>
-            <v-img
-                v-else
-                class="image"
-                :src="defaultImage"
-                alt="Generic Merchandise Image"
-            >
-            </v-img>
-            <p class="text-h5 text-center my-2">
-                {{ props.reward.name }}
-            </p>
-            <v-row class="ma-2 justify-center">
-                <v-btn
-                    color="primary"
-                    class="mr-2 cardButton"
-                    @click="emit('shop', props.reward.id)"
-                >
-                    <v-icon
-                        icon="mdi-shopping"
-                        color="text"
-                        size="x-large"
-                    ></v-icon>
-                </v-btn>
-                <v-btn
-                    color="warning"
-                    class="mr-2 cardButton"
-                    @click="emit('edit', props.reward.id)"
-                >
-                    <v-icon
-                        icon="mdi-pencil"
-                        color="text"
-                        size="x-large"
-                    ></v-icon>
-                </v-btn>
-                <v-btn
-                    color="danger"
-                    class="cardButton"
-                    @click="
-                        emit('delete', props.reward.id, props.reward.imageName)
-                    "
-                    ><v-icon
-                        icon="mdi-delete"
-                        color="text"
-                        size="x-large"
-                    ></v-icon
-                ></v-btn>
-            </v-row>
-        </v-card-text>
-    </v-card>
+  <v-card color="backgroundDarken" class="cardContainer">
+    <v-card-text>
+      <v-img
+        v-if="imageSrc"
+        class="image"
+        :src="imageSrc"
+        alt="Uploaded Image"
+      ></v-img>
+      <v-img
+        v-else
+        class="image"
+        :src="defaultImage"
+        alt="Generic Merchandise Image"
+      >
+      </v-img>
+      <p class="text-h5 text-center my-2">
+        {{ props.reward.name }}
+      </p>
+      <v-row class="ma-2 justify-center">
+        <v-btn
+          color="primary"
+          class="mr-2 cardButton"
+          @click="emit('shop', props.reward.id)"
+        >
+          <v-icon icon="mdi-shopping" color="text" size="x-large"></v-icon>
+        </v-btn>
+        <v-btn
+          color="warning"
+          class="mr-2 cardButton"
+          @click="emit('edit', props.reward.id)"
+        >
+          <v-icon icon="mdi-pencil" color="text" size="x-large"></v-icon>
+        </v-btn>
+        <v-btn
+          color="danger"
+          class="cardButton"
+          @click="emit('delete', props.reward.id, props.reward.imageName)"
+          ><v-icon icon="mdi-delete" color="text" size="x-large"></v-icon
+        ></v-btn>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <style scoped>
-    .cardContainer {
-        border-radius: 25px;
-    }
-    .cardButton {
-        border-radius: 13px;
-    }
-    .image {
-        max-height: 150px;
-    }
+.cardContainer {
+  border-radius: 25px;
+}
+.cardButton {
+  border-radius: 13px;
+}
+.image {
+  max-height: 150px;
+}
 </style>
--- src/views/admin/reward/RewardAddEditPage.vue
+++ src/views/admin/reward/RewardAddEditPage.vue
@@ -1,155 +1,147 @@
 <script setup>
-    import { onMounted, ref } from "vue";
-    import { useRouter, useRoute } from "vue-router";
-    import { required, positiveNumber } from "../../../utils/formValidators";
-    import rewardServices from "../../../services/rewardServices";
+import { onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { required, positiveNumber } from "../../../utils/formValidators";
+import rewardServices from "../../../services/rewardServices";
 
-    // Define statements for vue
-    const props = defineProps({
-        isAdd: Boolean
-    });
+// Define statements for vue
+const props = defineProps({
+  isAdd: Boolean,
+});
 
-    // Vue specific statements
-    const route = useRoute();
-    const router = useRouter();
+// Vue specific statements
+const route = useRoute();
+const router = useRouter();
 
-    // Reactive states
-    const errorMessage = ref("");
-    const form = ref(null);
-    const formData = ref({});
-    const image = ref(null);
+// Reactive states
+const errorMessage = ref("");
+const form = ref(null);
+const formData = ref({});
+const image = ref(null);
 
-    // Functions
-    const handleCancel = () => {
-        router.push({ name: "reward" });
-    };
+// Functions
+const handleCancel = () => {
+  router.push({ name: "reward" });
+};
 
-    const handleSubmit = async () => {
-        const isValid = (await form.value?.validate())?.valid;
-        if (!isValid) return;
-        try {
-            if (props.isAdd) {
-                await uploadImage();
-                await rewardServices.createReward(formData.value);
-            } else {
-                await handleImageUpdate();
-                await rewardServices.updateReward(
-                    route.params.id,
-                    formData.value
-                );
-            }
-            router.push({ name: "reward" });
-        } catch (error) {
-            errorMessage.value = "An error occurred while trying to submit.";
-            console.error("Error saving task:", error);
-        }
-    };
+const handleSubmit = async () => {
+  const isValid = (await form.value?.validate())?.valid;
+  if (!isValid) return;
+  try {
+    if (props.isAdd) {
+      await uploadImage();
+      await rewardServices.createReward(formData.value);
+    } else {
+      await handleImageUpdate();
+      await rewardServices.updateReward(route.params.id, formData.value);
+    }
+    router.push({ name: "reward" });
+  } catch (error) {
+    errorMessage.value = "An error occurred while trying to submit.";
+    console.error("Error saving task:", error);
+  }
+};
 
-    const uploadImage = async () => {
-        if (!image.value) return;
-        const response = await rewardServices.uploadRewardImage({
-            image: image.value
-        });
-        formData.value.imageName = response.data.fileName;
-    };
+const uploadImage = async () => {
+  if (!image.value) return;
+  const response = await rewardServices.uploadRewardImage({
+    image: image.value,
+  });
+  formData.value.imageName = response.data.fileName;
+};
 
-    const handleImageUpdate = async () => {
-        if (image.value && formData.value.imageName !== image.value.name) {
-            await rewardServices.deleteRewardImage(formData.value.imageName);
-            await uploadImage();
-        }
-        formData.value.image = undefined;
-    };
+const handleImageUpdate = async () => {
+  if (image.value && formData.value.imageName !== image.value.name) {
+    await rewardServices.deleteRewardImage(formData.value.imageName);
+    await uploadImage();
+  }
+  formData.value.image = undefined;
+};
 
-    // Vue functions
-    onMounted(async () => {
-        if (!props.isAdd) {
-            try {
-                let response = await rewardServices.getReward(route.params.id);
-                formData.value = response.data;
-                if (formData.value.imageName) {
-                    response = await rewardServices.getRewardImage(
-                        formData.value.imageName
-                    );
-                    image.value = new File(
-                        [response.data.image],
-                        formData.value.imageName
-                    );
-                }
-            } catch (err) {
-                console.log("Error", err);
-            }
-        }
-    });
+// Vue functions
+onMounted(async () => {
+  if (!props.isAdd) {
+    try {
+      let response = await rewardServices.getReward(route.params.id);
+      formData.value = response.data;
+      if (formData.value.imageName) {
+        response = await rewardServices.getRewardImage(
+          formData.value.imageName,
+        );
+        image.value = new File([response.data.image], formData.value.imageName);
+      }
+    } catch (err) {
+      console.log("Error", err);
+    }
+  }
+});
 </script>
 <template>
-    <v-alert closable v-if="errorMessage" type="error">
-        {{ errorMessage }}
-    </v-alert>
-    <h1 class="text-center ma-5">
-        {{ props.isAdd ? "Add Reward" : "Edit Reward" }}
-    </h1>
-    <v-form ref="form" @submit.prevent>
-        <v-container class="bg-backgroundDarken rounded-t-xl">
-            <v-text-field
-                v-model="formData.name"
-                variant="solo"
-                rounded="lg"
-                label="Name"
-                :rules="[required]"
-            ></v-text-field>
-            <v-row no-gutters>
-                <v-col size="6" class="mr-4">
-                    <v-text-field
-                        v-model="formData.points"
-                        variant="solo"
-                        rounded="lg"
-                        label="Points"
-                        :rules="[required, positiveNumber]"
-                    ></v-text-field>
-                </v-col>
-                <v-col size="6">
-                    <v-text-field
-                        v-model="formData.redemptionType"
-                        variant="solo"
-                        rounded="lg"
-                        label="Redemption Type"
-                        :rules="[required]"
-                    ></v-text-field>
-                </v-col>
-            </v-row>
-            <v-text-field
-                v-model="formData.redemptionInfo"
-                variant="solo"
-                rounded="lg"
-                label="Redemption Info"
-                :rules="[required]"
-            ></v-text-field>
-            <v-textarea
-                v-model="formData.description"
-                variant="solo"
-                rounded="lg"
-                label="Description"
-                :rules="[required]"
-            ></v-textarea>
-            <v-file-input
-                v-model="image"
-                variant="solo"
-                rounded="lg"
-                label="Image File"
-            ></v-file-input>
-            <v-row class="justify-center my-1">
-                <v-btn
-                    class="mr-2"
-                    variant="outlined"
-                    rounded="xl"
-                    @click="handleCancel"
-                    >Cancel</v-btn
-                >
-                <v-btn rounded="xl" color="primary" @click="handleSubmit"
-                    >Submit</v-btn
-                >
-            </v-row>
-        </v-container>
-    </v-form>
+  <v-alert v-if="errorMessage" closable type="error">
+    {{ errorMessage }}
+  </v-alert>
+  <h1 class="text-center ma-5">
+    {{ props.isAdd ? "Add Reward" : "Edit Reward" }}
+  </h1>
+  <v-form ref="form" @submit.prevent>
+    <v-container class="bg-backgroundDarken rounded-t-xl">
+      <v-text-field
+        v-model="formData.name"
+        variant="solo"
+        rounded="lg"
+        label="Name"
+        :rules="[required]"
+      ></v-text-field>
+      <v-row no-gutters>
+        <v-col size="6" class="mr-4">
+          <v-text-field
+            v-model="formData.points"
+            variant="solo"
+            rounded="lg"
+            label="Points"
+            :rules="[required, positiveNumber]"
+          ></v-text-field>
+        </v-col>
+        <v-col size="6">
+          <v-text-field
+            v-model="formData.redemptionType"
+            variant="solo"
+            rounded="lg"
+            label="Redemption Type"
+            :rules="[required]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-text-field
+        v-model="formData.redemptionInfo"
+        variant="solo"
+        rounded="lg"
+        label="Redemption Info"
+        :rules="[required]"
+      ></v-text-field>
+      <v-textarea
+        v-model="formData.description"
+        variant="solo"
+        rounded="lg"
+        label="Description"
+        :rules="[required]"
+      ></v-textarea>
+      <v-file-input
+        v-model="image"
+        variant="solo"
+        rounded="lg"
+        label="Image File"
+      ></v-file-input>
+      <v-row class="justify-center my-1">
+        <v-btn
+          class="mr-2"
+          variant="outlined"
+          rounded="xl"
+          @click="handleCancel"
+          >Cancel</v-btn
+        >
+        <v-btn rounded="xl" color="primary" @click="handleSubmit">Submit</v-btn>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
