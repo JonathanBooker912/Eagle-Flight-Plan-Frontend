46f3196ac7671baa9846e1f64ee159259832f963
Yorsh <george.hartman@eagles.oc.edu>
2025-03-03 14:28:15 -0600

    fixing linting errors

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router/router.js";
 import vuetify from "./plugins/vuetify.js";
 //import mdiVue from "mdi-vue/v3";
-import * as mdijs from "@mdi/js";
 import "@mdi/font/css/materialdesignicons.css";
 import { createPinia } from "pinia";
 import { userStore } from "./stores/userStore.js";
