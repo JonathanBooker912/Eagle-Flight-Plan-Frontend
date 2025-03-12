be1c31a606508811608227efd65a7162fc58eeed
Yorsh <george.hartman@eagles.oc.edu>
2025-03-12 14:25:32 -0500

    creating and formatting

--- src/router/router.js
+++ src/router/router.js
@@ -15,6 +15,7 @@ import RewardPage from "../views/admin/reward/RewardPage.vue";
 import RewardAddEditPage from "../views/admin/reward/RewardAddEditPage.vue";
 import RewardRedemptionPage from "../views/admin/reward/RewardRedemptionPage.vue";
 import MaintenanceLandingPage from "../views/admin/MaintenanceLandingPage.vue";
+import Profile from "../views/Profile.vue";
 import Notifications from "../views/Notification.vue";
 
 import AdminLanding from "../views/admin/AdminLanding.vue";
@@ -64,7 +65,7 @@ const router = createRouter({
         {
           path: "profile",
           name: "admin-profile",
-          component: AdminProfile,
+          component: Profile,
         },
         {
           path: "search",
@@ -237,7 +238,7 @@ const router = createRouter({
         {
           path: "profile",
           name: "faculty-profile",
-          component: FacultyProfile,
+          component: Profile,
         },
         {
           path: "search",
@@ -270,7 +271,7 @@ const router = createRouter({
         {
           path: "profile",
           name: "student-profile",
-          component: StudentProfile,
+          component: Profile,
         },
         {
           path: "search",
