8a4363a768b397a057f0af3baebc05ab5094c255
Yorsh <george.hartman@eagles.oc.edu>
2025-03-13 16:49:05 -0500

    strength card create

--- src/components/cards/StrengthCard.vue
+++ src/components/cards/StrengthCard.vue
@@ -14,67 +14,17 @@ const props = defineProps({
 </script>
 
 <template>
-  <v-card color="backgroundDarken" class="cardContainer">
-    <v-row no-gutters>
-      <v-col>
-        <v-card-text>
-          <p class="text-h5 text-no-wrap text-truncate">
-            {{ props.event.name }}
-          </p>
-          <p
-            class="text-subtitle-1 font-weight-regular text-no-wrap text-truncate"
-          >
-            {{ props.event.location || "No Location" }}
-          </p>
-          <p class="text-subtitle-1 font-weight-regular">
-            {{ eventDate }}
-          </p>
-          <p class="text-subtitle-1 font-weight-regular">
-            {{ eventTime }}
-          </p>
-        </v-card-text>
-        <v-row class="ma-2 float-right">
-          <v-btn
-            color="warning"
-            class="mr-2 cardButton elevation-0"
-            @click="editEvent"
-          >
-            <v-icon icon="mdi-pencil" color="text" size="x-large"></v-icon>
-          </v-btn>
-          <v-btn
-            color="danger"
-            class="cardButton elevation-0"
-            @click="emit('delete', props.event.id)"
-            ><v-icon icon="mdi-delete" color="text" size="x-large"></v-icon
-          ></v-btn>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-card>
-  <v-card
-    v-else
-    color="backgroundDarken"
-    class="cardContainer"
-    @click="viewCard"
-  >
-    <v-row no-gutters>
-      <div class="h-fill left-accent my-2 ml-2 bg-primary"></div>
-      <v-col>
-        <v-card-text>
-          <p class="text-h5 text-truncate w-100">
-            {{ props.event.name }}
-          </p>
-          <p class="text-subtitle-1 font-weight-regular">
-            {{ props.event.location }}
-          </p>
-          <p class="text-subtitle-1 font-weight-regular">
-            {{ eventDate }}
-          </p>
-          <p class="text-subtitle-1 font-weight-regular">
-            {{ eventTime }}
-          </p>
-        </v-card-text>
-      </v-col>
+  <v-card color="backgroundDarken">
+    <v-row align="center">
+      <v-typography class="mr-3">
+        {{ props.strength.number }}
+      </v-typography>
+      <v-typography class="mr-3">
+        {{ props.strength.name }}
+      </v-typography>
+      <v-typography class="mr-3">
+        {{ props.strength.domain }}
+      </v-typography>
     </v-row>
   </v-card>
 </template>
