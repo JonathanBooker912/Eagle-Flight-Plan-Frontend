12e3a8bfe8b61b82b74709fa475b8f409407d94f
Yorsh <george.hartman@eagles.oc.edu>
2025-03-24 15:48:51 -0500

    Adding the descriptions to the cards

--- src/components/cards/StrengthCard.vue
+++ src/components/cards/StrengthCard.vue
@@ -1,3 +1,34 @@
+<template>
+  <v-card
+    v-show="hoveredStrength === null || hoveredStrength === strength.name"
+    color="backgroundDarken"
+    class="pa-3 my-2 rounded-lg strengthCard"
+    :class="{
+      noShow: hoveredStrength !== strength.name && hoveredStrength !== null, // Default class when not hovered
+      expanded: hoveredStrength === strength.name, // Expanded class when hovered
+    }"
+    @mouseover="hoveredStrength = strength.name"
+    @mouseleave="hoveredStrength = null"
+  >
+    <v-row align="center">
+      <!-- Strength number in category -->
+      <v-card class="category" :style="{ backgroundColor: categoryColor }">
+        <h3>{{ props.strength.number }}</h3>
+      </v-card>
+      <h3>{{ props.strength.name }}</h3>
+      <p class="ml-auto domain">{{ props.strength.domain }}</p>
+    </v-row>
+
+    <!-- Conditionally show the description when hovered -->
+    <p
+      v-if="hoveredStrength === props.strength.name"
+      class="strength-description"
+    >
+      {{ strengthDescription }}
+    </p>
+  </v-card>
+</template>
+
 <script setup>
 import { ref, computed } from "vue";
 
@@ -27,6 +58,16 @@ const categoryColor = computed(() => {
 
   return domainColors[props.strength.domain] || "#0070CA"; // Default to blue if no match
 });
+
+// Handle mouse over event to set hovered strength
+const handleHover = () => {
+  hoveredStrength.value = props.strength.name;
+};
+
+// Handle mouse leave event to reset hovered strength
+const handleLeave = () => {
+  hoveredStrength.value = null;
+};
 </script>
 
 <template>
@@ -116,5 +157,14 @@ const categoryColor = computed(() => {
   font-size: 1rem;
   line-height: 1.5;
   color: rgba(255, 255, 255, 0.9);
+}
+
+.noShow {
+  display: none;
+  height: 0%;
+}
+
+.expanded {
+  height: 100%;
 }
 </style>
