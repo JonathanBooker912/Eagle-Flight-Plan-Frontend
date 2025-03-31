3038d8a752f315970159f38227eff178076afb51
Yorsh <george.hartman@eagles.oc.edu>
2025-03-31 15:00:19 -0500

    Starting on page

--- src/views/student/StudentLanding.vue
+++ src/views/student/StudentLanding.vue
@@ -1,5 +1,43 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import notificationServices from "../../services/notificationServices";
+import NotificationCard from "../../components/cards/NotificationCard.vue";
+
+import { userStore } from "../../stores/userStore";
+const route = useRoute();
+
+const notifications = ref([]);
+const selectedNotif = ref({});
+const showsidebar = ref(false);
+
+const currentPage = ref(1);
+const pageSize = ref(14);
+const totalPages = ref(1);
+const store = userStore();
+
+const getNotifications = async (page = 1) => {
+  try {
+    const res = await notificationServices.getAllNotificationsForUser(
+      store.user.userId,
+      page,
+      pageSize.value,
+    );
+
+    notifications.value = res.data.notifications; // Update the notifications array
+    totalPages.value = Math.ceil(res.data.total / pageSize.value);
+    currentPage.value = page; // Ensure currentPage updates correctly
+
+    console.log("Updated Notifications:", notifications.value); // Debugging: Check if notifications update
+  } catch (err) {
+    console.error("Error fetching notifications:", err);
+  }
+};
+
+onMounted(() => {
+  getNotifications();
+});
+
 </script>
 
 <template>
@@ -7,45 +45,38 @@ import { RouterView } from "vue-router";
     <p>This is where the bar will go</p>
   </v-card>
   <v-row>
-    <v-card color="backgroundDarken" class="adminItem adminItemSmall">
+    <v-card color="backgroundDarken" class="adminItem">
+      <strong style="font-size: 20px; padding-bottom: 5px">Flight Plan</strong>
+    </v-card>
+    <v-card color="backgroundDarken" class="adminItem">
       <strong style="font-size: 20px; padding-bottom: 5px"
-        >Upcoming Events</strong
-      >
+        >Notifications</strong>
+        <NotificationCard
+          v-for="(item, index) in notifications"
+          :key="index"
+          :to="{ name: 'student-notifications' }"
+          :notification="item"
+          :class="{ unread: !item.read, read: item.read }"
+          style="height: 10vh; border-color: white;"
+          @click="editItem(item)"
+        />
     </v-card>
-    <v-card color="backgroundDarken" class="adminItem adminItemBig">
-      <p>Engagement Breakdown by Classification</p>
-    </v-card>
-    <v-card color="backgroundDarken" class="adminItem adminItemSmall">
-      <strong style="font-size: 20px; padding-bottom: 5px"
-        >Notifications</strong
-      >
-    </v-card>
-    <v-card color="backgroundDarken" class="adminItem adminItemBig">
-      <p>Percentage of students on track to complete their flight plan</p>
+    <v-card color="backgroundDarken" class="adminItem">
+      <strong style="font-size: 20px; padding-bottom: 5px">Calendar</strong>
     </v-card>
   </v-row>
 </template>
 
 <style>
-.main-back {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0.5%;
-  padding: 0.5%;
-  flex-wrap: wrap;
-  align-self: flex-end;
-  border-radius: 25px;
-  width: 90vw;
-}
-
 .adminItem {
   display: flex;
   flex-direction: column;
   text-align: left;
   margin: 1%;
-  height: 45vh;
+  height: 80vh;
   padding: 15px 0px 5px 0px;
+  width: 25vw;
+  text-align: center;
 
   border-radius: 25px;
 }
