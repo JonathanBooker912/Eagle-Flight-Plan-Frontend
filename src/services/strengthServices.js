3e5933964c9acd31b66bf734c77a37f56f3aebdc
KyleD0711 <kyle.denney@eagles.oc.edu>
2025-04-05 15:40:26 -0500

    Fixed rebasing error

--- src/services/strengthServices.js
+++ src/services/strengthServices.js
@@ -1,7 +1,10 @@
 import apiClient from "./services.js";
 
 export default {
-  getStrengthsForStudent(studentId) {
-    return apiClient.get(`/strengths/student/${studentId}`);
-  },
+    getStrengthsForStudent(studentId) {
+        return apiClient.get(`/strengths/student/${studentId}`);
+    },
+    getAllStrengths() {
+        return apiClient.get(`/strengths`);
+    }
 };
