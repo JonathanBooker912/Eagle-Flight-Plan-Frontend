3c50aa4dd301eec0c49b03c08d9bf14ac8681303
Jonathan Booker <jonathan.booker@eagles.oc.edu>
2025-04-14 23:54:43 -0500

    added major maintenance

--- src/router/router.js
+++ src/router/router.js
@@ -15,6 +15,8 @@ import RewardPage from "../views/admin/reward/RewardPage.vue";
 import RewardAddEditPage from "../views/admin/reward/RewardAddEditPage.vue";
 import RewardRedemptionPage from "../views/admin/reward/RewardRedemptionPage.vue";
 import MaintenanceLandingPage from "../views/admin/MaintenanceLandingPage.vue";
+import MajorsPage from "../views/admin/major/MajorsPage.vue";
+import MajorAddEditPage from "../views/admin/major/MajorAddEditPage.vue";
 import Profile from "../views/Profile.vue";
 import Notifications from "../views/Notification.vue";
 
@@ -194,6 +196,25 @@ const adminFlightPlanRoutes = [
     component: AddFlightPlanItem,
   },
 ];
+const adminMajorRoutes = [
+  {
+    path: "maintenance/major",
+    name: "majors",
+    component: MajorsPage,
+  },
+  {
+    path: "maintenance/major/edit/:id",
+    name: "editMajor",
+    component: MajorAddEditPage,
+    props: { isAdd: false },
+  },
+  {
+    path: "maintenance/major/add",
+    name: "addMajor",
+    component: MajorAddEditPage,
+    props: { isAdd: true },
+  },
+];
 
 const adminMaintenanceRoutes = [
   {
@@ -218,6 +239,7 @@ const adminMaintenanceRoutes = [
   ...adminEventRoutes,
   ...adminBadgeRoutes,
   ...adminFlightPlanRoutes,
+  ...adminMajorRoutes,
 ];
 
 const facultyRoutes = [
