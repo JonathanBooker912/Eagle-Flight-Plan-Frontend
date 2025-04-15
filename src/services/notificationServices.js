3dfc97a77c00cb0e6d08a33bcbbda09357578820
Yorsh <george.hartman@eagles.oc.edu>
2025-04-14 22:43:44 -0500

    Fixing order of notifications

--- src/services/notificationServices.js
+++ src/services/notificationServices.js
@@ -3,8 +3,16 @@ import apiClient from "./services.js";
 export default {
   getAllNotificationsForUser(id, page = 1, pageSize = 10) {
     return apiClient.get(`/notification/user/${id}`, {
-      params: { page, pageSize },
+      params: { 
+        page, 
+        pageSize,
+        sortBy: 'createdAt',
+        sortOrder: 'desc'
+      },
     });
+  },
+  getAllNotificationsForUserWithoutPagination(id) {
+    return apiClient.get(`/notification/user/${id}/all`);
   },
   createNotification(notificationData) {
     return apiClient.post("/notification", notificationData);
