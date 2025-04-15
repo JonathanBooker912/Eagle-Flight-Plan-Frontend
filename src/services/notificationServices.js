52d5f44c1a6eb460a3e33c525fcb6c8f64d9a045
Yorsh <george.hartman@eagles.oc.edu>
2025-04-14 22:44:22 -0500

    format

--- src/services/notificationServices.js
+++ src/services/notificationServices.js
@@ -3,11 +3,11 @@ import apiClient from "./services.js";
 export default {
   getAllNotificationsForUser(id, page = 1, pageSize = 10) {
     return apiClient.get(`/notification/user/${id}`, {
-      params: { 
-        page, 
+      params: {
+        page,
         pageSize,
-        sortBy: 'createdAt',
-        sortOrder: 'desc'
+        sortBy: "createdAt",
+        sortOrder: "desc",
       },
     });
   },
