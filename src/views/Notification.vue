7a2a0745f9eba71c9e76ac86b113553053eccc1e
Yorsh <george.hartman@eagles.oc.edu>
2025-04-21 14:54:31 -0500

    format

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -220,7 +220,12 @@ const closeDialogs = () => {
       >
         <v-icon>mdi-close</v-icon>
       </v-btn>
-      <p>Sent By: {{ selectedNotif.user ? selectedNotif.user.fullName : "Eagle Flight Plan" }}</p>
+      <p>
+        Sent By:
+        {{
+          selectedNotif.user ? selectedNotif.user.fullName : "Eagle Flight Plan"
+        }}
+      </p>
       <p>Sent On: {{ formattedDateTime(selectedNotif.createdAt) }}</p>
       <p>
         ------------------------------------------------------------------------
