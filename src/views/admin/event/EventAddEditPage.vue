52221e6e6d185022fc1e91bab485dcab08bdf96a
jonahveit2002 <97561214+jonahveit2002@users.noreply.github.com>
2025-04-23 13:33:46 -0500

    lint

--- src/views/admin/event/EventAddEditPage.vue
+++ src/views/admin/event/EventAddEditPage.vue
@@ -64,7 +64,7 @@ const handleSubmit = async () => {
     console.error(
       "Invalid time values:",
       formData.value.startTime,
-      formData.value.endTime
+      formData.value.endTime,
     );
     return;
   }
@@ -121,7 +121,7 @@ const filteredEndTimeOptions = computed(() => {
   if (!formData.value.startTime) return timeOptions.value;
   const start = parseTimeString(formData.value.startTime, "01/01/2000");
   return timeOptions.value.filter(
-    (time) => parseTimeString(time, "01/01/2000") > start
+    (time) => parseTimeString(time, "01/01/2000") > start,
   );
 });
 
